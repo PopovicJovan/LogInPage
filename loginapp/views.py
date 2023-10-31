70350c5031b1f7d40bb17f96b6527a38dfbe566b
PopovicJovan <jovanp.school06@gmail.com>
2023-10-31 23:18:46 +0100

    token is finished, login and signin and frontend for login and signin page is finished

--- loginapp/views.py
+++ loginapp/views.py
@@ -3,35 +3,45 @@ from .serializers import UserSerializer
 from .models import User
 from rest_framework.response import Response
 import jwt
+from django.contrib.auth.hashers import make_password, check_password
 
 
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
 
-    def get_queryset(self):
-        return User.objects.all()
+    def get_queryset(self): return User.objects.all()
 
-    def destroy(self, request, *args, **kwargs):
+    def destroy(self, request, *args, **kwargs): return Response()
+
+    def update(self, request, *args, **kwargs): return Response()
+    def create(self, request, *args, **kwargs):
+        first_name = request.data['first_name']
+        last_name = request.data['last_name']
+        eamil = request.data['email']
+        password = make_password(request.data['password'])
+        User.objects.create(email=eamil,
+                            first_name=first_name,
+                            last_name=last_name,
+                            password=password)
         return Response()
 
-    def update(self, request, *args, **kwargs):
-        return Response()
 
 
 class LogInSet(ModelViewSet):
     serializer_class = UserSerializer
 
-    def get_queryset(self):
-        return User.objects.all()
+    def get_queryset(self):return User.objects.all()
 
     def create(self, request, *args, **kwargs):
         try:
-            user = User.objects.get(first_name=request.data['first_name'],
-                                    password=request.data['password'])
+            user = User.objects.get(first_name=request.data['first_name'])
+            if not check_password(request.data['password'], user.password):
+                return Response('Credentials are wrong!')
 
             payload_data = {'username:': user.first_name,
-                            'password:': user.password  }
+                            'password:': user.password}
+
             token = jwt.encode(payload_data,
                                'secret')
             return Response(token)
