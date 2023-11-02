b0ecbfda14b921777eacbe7c3e01b3f2e4523c73
PopovicJovan <jovanp.school06@gmail.com>
2023-11-02 12:23:18 +0100

    last commit

--- loginapp/models.py
+++ loginapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
+
 class User(models.Model):
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
-    email = models.EmailField()
+    username = models.CharField(max_length=32, unique=True)
     password = models.CharField(max_length=64)
-
--- loginapp/views.py
+++ loginapp/views.py
@@ -6,7 +6,6 @@ import jwt
 from django.contrib.auth.hashers import make_password, check_password
 
 
-
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
 
@@ -15,36 +14,35 @@ class UserViewSet(ModelViewSet):
     def destroy(self, request, *args, **kwargs): return Response()
 
     def update(self, request, *args, **kwargs): return Response()
+
     def create(self, request, *args, **kwargs):
         first_name = request.data['first_name']
         last_name = request.data['last_name']
-        eamil = request.data['email']
+        username = request.data['username']
         password = make_password(request.data['password'])
-        User.objects.create(email=eamil,
+        User.objects.create(username=username,
                             first_name=first_name,
                             last_name=last_name,
                             password=password)
         return Response()
 
 
-
 class LogInSet(ModelViewSet):
     serializer_class = UserSerializer
 
-    def get_queryset(self):return User.objects.all()
+    def get_queryset(self): return User.objects.all()
 
     def create(self, request, *args, **kwargs):
         try:
-            user = User.objects.get(first_name=request.data['first_name'])
+            user = User.objects.get(username=request.data['username'])
             if not check_password(request.data['password'], user.password):
                 return Response('Credentials are wrong!')
 
-            payload_data = {'username:': user.first_name,
-                            'password:': user.password}
+            payload_data = {'username:': user.username,
+                            'first name': user.first_name,
+                            'last name': user.last_name}
 
             token = jwt.encode(payload_data,
                                'secret')
             return Response(token)
         except User.DoesNotExist: return Response({'Wrong info!'})
-
-
